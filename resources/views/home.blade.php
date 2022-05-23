9de6e0c94fc11dd09321663ff3f7ddca72679dfc
Deshan C Handapangoda <50766862+DeshanCharith@users.noreply.github.com>
2022-05-23 22:49:21 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,8 @@
             <div class="card">
                 <div class="card-header">Create Sales Representative:</div>
 
-                <div class="card-body">
-                @if (session('status'))
+              <div class="card-body">
+                    @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
@@ -36,23 +36,36 @@
                           <label>Telephone</label>
                           <input type="text" name="telephone" class="form-control" placeholder="Enter " required >
                         </div>
-
-                        
-                        <div class="form-group">
+   
+                        <!-- <div class="form-group">
                           <label>Joined date</label>
                           <input type="text" name="joined_date" class="form-control" placeholder="Enter post title" required >
-                
+                        </div> -->
+                        <div class="form-group">
+                          <label>Joined date</label>
+                        <input type="text"  class="form-control" name="joined_date" {{ Form::text('date', '', array('id' => 'datepicker')) }}
                         </div>
-                          <div class="form-group">
-                          <label>Current routes</label>
-                          <input type="text" name="current_routes" class="form-control" placeholder="Enter" required >
+
+                        <div class="mb-3 mt-3 form-group" style="">
+                            <label for="current_routes" class="form-label">Route</label>
+                            <select style="" name="current_routes" class="custom-select custom-select-sm form-control"> 
+                                <option selected>Select Route</option>
+                               
+                                <option value="Cloombo 1 ">Cloombo 1 </option>
+                                <option value="Cloombo 2 ">Cloombo 2</option>
+                                <option value="Cloombo 3 ">Cloombo 3</option>
+                                <option value="Cloombo 4 ">Cloombo 4</option> 
+        
+                            </select>
                         </div>
+
                         <div class="form-group">
                           <label>Comment</label>
-                          <input type="text" name="comment" class="form-control" placeholder="Enter " required >
-                        </div>
+                          <textarea class="form-control" name="comment" placeholder="Enter post description" rows="6"  placeholder="Enter "></textarea>
                         </div>
                         <button type="submit" class="btn btn-primary mt-3">Create</button>
+                        </div>
+                       
                     </form>
                 </div>
             </div>
