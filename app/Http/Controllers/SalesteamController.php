179252a78d3553d30b1a120ee70598f1b64cf378
Deshan C Handapangoda <50766862+DeshanCharith@users.noreply.github.com>
2022-05-23 22:54:58 +0530

    Update SalesteamController.php

--- app/Http/Controllers/SalesteamController.php
+++ app/Http/Controllers/SalesteamController.php
@@ -8,12 +8,15 @@ use Illuminate\Support\Facades\Validator;
 
 class SalesteamController extends Controller
 {
-    public function createTeam(Request $request) {
+
+//Create team member
+public function createTeam(Request $request) {
        
         $validation = Validator::make($request->all(), [
             'name' => 'required|string',
-            'email' => 'required|string',
-            'telephone' => 'required|string',
+            'email' => 'required|email|unique:salesteams,email',
+            // 'telephone' => 'required|unique:salesteams,telephone|numeric|min:8|max:11',
+            'telephone' => 'required|unique:salesteams,telephone|numeric|digits:9',
             'joined_date' => 'required|string',
             'current_routes' => 'required|string',
             'comment' => 'required|string',
@@ -21,19 +24,19 @@ class SalesteamController extends Controller
         ]);
 
         if($validation->fails()) {
-            return back()->with('error', 'Something went wrong!');
+            return back()->with('error', $validation->errors());
         } 
         
         else {
         
-            // $date=date_create($request->joined_date);
-            // echo date_format($date,"m-d-Y H:i:s");
+            $date=date_create($request->joined_date);
+            $date= date_format($date,"m-d-Y");
 
             Salesteam::create([
             'name' => $request->name,
             'email' => $request->email,
             'telephone' =>$request->telephone,
-            'joined_date' => $request->joined_date,
+            'joined_date' => $date,
             'current_routes' => $request->current_routes,
             'comment' =>$request->comment
             ]);
@@ -41,39 +44,65 @@ class SalesteamController extends Controller
 
        return redirect(route('team.all'))->with('status', 'Member created successfully!');
 
-    }
+}
 
-    public function getTeam(){
+//Get all records of team member
+public function getTeam(){
         $teams = Salesteam::paginate(5);
         return view('Teamview', compact('teams'));
-    }
+}
 
-    public function editTeam($Id) {
+//Navigate to team edit page with deatils of editable
+public function editTeam($Id) {
         $team = Salesteam::findOrFail($Id);
         return view('Teamedit', compact('team'));
-    }
+}
 
-    public function updateTeam($Id, Request $request) {
+//Update team member details
+public function updateTeam($Id, Request $request) {
+
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:salesteams,email,'.$Id,
+            // 'telephone' => 'required|numeric|min:8|max:12|unique:salesteams,telephone,'.$Id,
+            'telephone' => 'required|numeric|digits:9|unique:salesteams,telephone,'.$Id,
+            'joined_date' => 'required|string',
+            'current_routes' => 'required|string',
+            'comment' => 'required|string',
+          
+        ]);
+
+        if($validation->fails()) {
+            // print_r($validation1->errors());
+            // die("jhh");
+            return back()->with('error', $validation->errors());
+        } 
+
+        else {
+
+            $date=date_create($request->joined_date);
+            $date= date_format($date,"m-d-Y");
 
         Salesteam::where('id',$Id)->update([
             'id' =>$Id,
             'name' => $request->name,
             'email' => $request->email,
             'telephone' =>$request->telephone,
-            'joined_date' => $request->joined_date,
+            'joined_date' => $date,
             'current_routes' => $request->current_routes,
             'comment' =>$request->comment
         ]);
-
+        }
         return redirect(route('team.all'))->with('status', 'Member updated!');
-    }
+}
 
-    public function deleteTeam($Id) {
+//Delete team member
+public function deleteTeam($Id) {
         Salesteam::findOrFail($Id)->delete();
         return redirect(route('team.all'))->with('status', 'Member deleted!');
 
 
-    }
+}
     
 
 }
